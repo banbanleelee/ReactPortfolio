88e24b0c7385cd9b24203d2ebcb11e54bd1970f8
sally <sallyhoney96@gmail.com>
2022-06-17 00:51:00 -0400

    fixed typo

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/banbanleelee/ReachPortfolio/main/src/Assets/Yixuan_Sally_Zhang_Resume.pdf";
+  "https://raw.githubusercontent.com/banbanleelee/ReactPortfolio/main/src/Assets/Yixuan_Sally_Zhang_Resume.pdf";
 
   function ResumeNew() {
   const [width, setWidth] = useState(1200);
